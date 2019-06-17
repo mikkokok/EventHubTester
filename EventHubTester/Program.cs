2e1737f89449f42fd02757f457cb32b22bc83111
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2019-06-17 19:11:36 +0300

    Add project files.

---
+++ EventHubTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHubTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
